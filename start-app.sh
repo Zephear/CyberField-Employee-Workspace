09d5cf6513f3843d95029a621bab515f7076710b
Vladyslav Dovhyi <dovhuy5@gmail.com>
2026-03-31 08:09:37 +0200

    first

---
+++ start-app.sh
@@ -0,0 +1,5 @@
+echo "Starting ZephyrusITee application..."
+docker-compose up -d --build
+echo "ZephyrusITee application is running."
+echo "Frontend: http://localhost:8080"
+echo "Backend API: http://localhost:3000"
